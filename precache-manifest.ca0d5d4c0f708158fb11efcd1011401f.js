9ebc174a2dbe1167139ff8a23ea2bbc114809ee8
Detnex <manjeetit3026@gmail.com>
2019-07-23 20:23:35 +0530

    Updates

---
+++ precache-manifest.ca0d5d4c0f708158fb11efcd1011401f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c3a0f924e6138b143167755e9990896",
+    "url": "/stopwatch/index.html"
+  },
+  {
+    "revision": "95ceb53f7898c1ee79bd",
+    "url": "/stopwatch/static/css/main.a0aec872.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/stopwatch/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "95ceb53f7898c1ee79bd",
+    "url": "/stopwatch/static/js/main.73636285.chunk.js"
+  },
+  {
+    "revision": "24a3b63eeedaf9f526ca",
+    "url": "/stopwatch/static/js/runtime~main.000e15a5.js"
+  }
+]);
